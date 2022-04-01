f1edb95950c7fd0c60dad9fffd39002dab915b77
Paula Nguyen <nguyen.paula5560@gmail.com>
2022-04-01 12:03:29 -0700

    commited assignment

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) The line below this just says that BlogPostsController is inherinting from ApplicationController
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) You're creating an instance variable that will show all the data entries for that model
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3)In this method show, which normally shows a URL at a time, is finding a certain entry that corresponds with the primary key
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4)the route for new will offers a form that create new item. It'll create a form but it will not create a new entry of data. that's where create comes from.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5)when you are creating and saving actual data into your database you are going to make sure that it is valid. You wouldn't want any invalid data saved into your database. this goes for line 7 as well. you notice that in the parenthesis there is a blog post params. That is defined at the bottom of the page. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) When editing, you're wondering which one to edit. since there are so many you would need the param because you'd be able to call on any (dynamic). then you would edit from there.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)Right here you are using validations and basically it will only accept data that is valid. You would only want valid data entered into the database.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) In the destroy method it is going to delete something. Once it's deleted at the code below it will redirect you to the "home" page.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) without the word private from line from below so that it won't be accessible by your user
   private
   def blog_post_params
-    # ---10)
+    # ---10) What this line is saying is... "hey you need this in order to pass but if you don't I will throw an error". It forces the user to comply.
     params.require(:blog_post).permit(:title, :content)
   end
 end
